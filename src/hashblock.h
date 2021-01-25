c60cd791ee5ed6c720cf9837beb139f4be833858
kkymco <93065587+kkymco@users.noreply.github.com>
2021-01-25 10:24:01 +0000

    Fix minor warnings

---
+++ src/hashblock.h
@@ -0,0 +1,19 @@
+#ifndef HASHBLOCK_H
+#define HASHBLOCK_H
+
+#include "uint256.h"
+#include "sph_blake.h"
+#include "sph_bmw.h"
+#include "sph_groestl.h"
+#include "sph_jh.h"
+#include "sph_keccak.h"
+#include "sph_skein.h"
+#include "sph_luffa.h"
+#include "sph_cubehash.h"
+#include "sph_shavite.h"
+#include "sph_simd.h"
+#include "sph_echo.h"
+
+#ifndef QT_NO_DEBUG
+#include <string>
+#endi
