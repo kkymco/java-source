223db5c6bba4ab5c1d8c046dd4bbffad93951366
kkymco <93065587+kkymco@users.noreply.github.com>
2021-05-16 22:33:24 +0000

    Add Typescript definitions

---
+++ src/leveldb/include/leveldb/cache.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// A Cache is an interface that maps keys to values.  It has internal
+// synchronization and may be safely accessed concurrently from
+// multiple threads.  It may automatically evict entries to make room
+// for new entries.  Values have a specified charge against the cache
+// capacity.  For example, a cache where the values are variable
+// length strings, may use the length of the string as the charge for
+// the string.
+//
+// A builtin cache implementation with a least-recently-used eviction
+// policy is provided.  Clients may use their own implementations 
