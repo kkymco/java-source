003f4f9c270d04e1eae93bd86f5f1fd7e1fe7e95
kkymco <93065587+kkymco@users.noreply.github.com>
2021-05-19 07:11:55 +0000

    Push all updates

---
+++ src/leveldb/include/leveldb/env.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// An Env is an interface used by the leveldb implementation to access
+// operating system functionality like the filesystem etc.  Callers
+// may wish to provide a custom Env object when opening a database to
+// get fine gain control; e.g., to rate limit file system operations.
+//
+// All Env implementations are safe for concurrent access from
+// multiple threads without any external synchronization.
+
+#ifndef STORAGE_LEVELDB_INCLUDE_ENV_H_
+#define STORAGE_LEVELD
