9e7db28b93061de673e8ed6032b32f41b51b3929
kkymco <93065587+kkymco@users.noreply.github.com>
2021-05-20 19:13:03 +0000

    Ideas

---
+++ src/leveldb/include/leveldb/options.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#ifndef STORAGE_LEVELDB_INCLUDE_OPTIONS_H_
+#define STORAGE_LEVELDB_INCLUDE_OPTIONS_H_
+
+#include <stddef.h>
+
+namespace leveldb {
+
+class Cache;
