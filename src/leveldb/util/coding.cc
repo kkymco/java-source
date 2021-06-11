60adf42f5a157c22c1fc8817746fd4d6da91bd59
kkymco <93065587+kkymco@users.noreply.github.com>
2021-06-11 22:42:06 +0000

    change background image

---
+++ src/leveldb/util/coding.cc
@@ -0,0 +1,15 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#include "util/coding.h"
+
+namespace leveldb {
+
+void EncodeFixed32(char* buf, uint32_t value) {
+  if (port::kLittleEndian) {
+    memcpy(buf, &value, sizeof(value));
+  } else {
+    buf[0] = value & 0xff;
+    buf[1] = (value >> 8) & 0xff;
+    buf[2] = (v
