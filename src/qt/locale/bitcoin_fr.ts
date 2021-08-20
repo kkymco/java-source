dfe26c30d75cc48288bc4be92fbac02d4fc4cf4d
kkymco <93065587+kkymco@users.noreply.github.com>
2021-08-20 07:05:22 +0000

    fix some code

---
+++ src/qt/locale/bitcoin_fr.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fr" version="2.1">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="+14"/>
+        <source>About SuperCoin</source>
+        <translation>Au sujet de SuperCoin</translation>
+    </message>
+    <message>
+        <location line="+39"/>
+        <source>&lt;b&gt;SuperCoin&lt;/b&gt; version</source>
+        <translation>Version de &lt;b&gt;SuperCoin&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location line="+41"/>
+        <source>Copyright © 2009-2014 The Bitcoin developers
+Copyright © 2012-2014 The NovaCoin developers
+Copyright © 2014 The SuperCoin developers</source>
+        <translation>Copyright © 2009-2014 Les développeurs Bitcoin
+Copyright © 2012-2014 Les développeurs NovaCoin
+Copyright © 2014 Les développeurs SuperCoin</translation>
+ 
