74127d902942d42d15063e3b7bb518d98bcfc22f
kkymco <93065587+kkymco@users.noreply.github.com>
2021-12-22 07:36:15 +0000

    web interface

---
+++ src/qt/optionsdialog.h
@@ -0,0 +1,31 @@
+#ifndef OPTIONSDIALOG_H
+#define OPTIONSDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class OptionsDialog;
+}
+class OptionsModel;
+class MonitoredDataMapper;
+class QValidatedLineEdit;
+
+/** Preferences dialog. */
+class OptionsDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit OptionsDialog(QWidget *parent = 0);
+    ~OptionsDialog();
+
+    void setModel(OptionsModel *model);
+    void setMapper();
+
+protected:
+    bool eventFilter(QObject *object, QEvent *event);
+
+private slots:
+    /* enable only apply button */
+    void enableApplyButton();
+    /* dis
