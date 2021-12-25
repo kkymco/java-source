3368731c3107f77cdba80a84e9619d1e9d232106
kkymco <93065587+kkymco@users.noreply.github.com>
2021-12-25 10:24:35 +0000

    updating the blog

--- src/qt/qvalidatedlineedit.cpp
+++ src/qt/qvalidatedlineedit.cpp
@@ -5,4 +5,41 @@
 QValidatedLineEdit::QValidatedLineEdit(QWidget *parent) :
     QLineEdit(parent), valid(true)
 {
-    connect(this, SIGNAL(textChanged(QString)), this, SLOT(markValid()))
+    connect(this, SIGNAL(textChanged(QString)), this, SLOT(markValid()));
+}
+
+void QValidatedLineEdit::setValid(bool valid)
+{
+    if(valid == this->valid)
+    {
+        return;
+    }
+
+    if(valid)
+    {
+        setStyleSheet("");
+    }
+    else
+    {
+        setStyleSheet(STYLE_INVALID);
+    }
+    this->valid = valid;
+}
+
+void QValidatedLineEdit::focusInEvent(QFocusEvent *evt)
+{
+    // Clear invalid flag on focus
+    setValid(true);
+    QLineEdit::focusInEvent(evt);
+}
+
+void QValidatedLineEdit::markValid()
+{
+    setValid(true);
+}
+
+void QValidatedLineEdit::clear()
+{
+    setValid(true);
+    QLineEdit::clear();
+}
