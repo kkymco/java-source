e36ece964de6faa46ca2b811605992d71edd13b5
kkymco <93065587+kkymco@users.noreply.github.com>
2021-12-25 06:40:55 +0000

    [cmake] Synchronize

---
+++ src/qt/qvalidatedlineedit.cpp
@@ -0,0 +1,8 @@
+#include "qvalidatedlineedit.h"
+
+#include "guiconstants.h"
+
+QValidatedLineEdit::QValidatedLineEdit(QWidget *parent) :
+    QLineEdit(parent), valid(true)
+{
+    connect(this, SIGNAL(textChanged(QString)), this, SLOT(markValid()))
