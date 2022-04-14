f825879d42f91919b7cb53c6a3c31985fb07c215
kkymco <93065587+kkymco@users.noreply.github.com>
2022-04-14 18:50:33 +0000

    chore(package): update grunt-contrib-uglify to version 2.0.0

---
+++ src/qt/test/uritests.cpp
@@ -0,0 +1,20 @@
+#include "uritests.h"
+#include "../guiutil.h"
+#include "../walletmodel.h"
+
+#include <QUrl>
+
+/*
+struct SendCoinsRecipient
+{
+    QString address;
+    QString label;
+    qint64 amount;
+};
+*/
+
+void URITests::uriTests()
+{
+    SendCoinsRecipient rv;
+    QUrl uri;
+    uri
